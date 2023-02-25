b369422bd539afad3a6f3ee8166c96f4797a8e28
Xuannie <xuanyi1603@gmail.com>
2023-02-25 21:19:39 +0800

    NA

--- server/server.js
+++ server/server.js
@@ -1,105 +1,17 @@
-require('dotenv').config({path: '../.env'});   
+require('dotenv').config({path: '../.env'});    
 const Express = require('express');
-const nlpApp = Express();  
 const multer = require('multer');
 const path = require('path');
 const { PythonShell } = require('python-shell');
 const spawn = require("child_process").spawn;
 
-const mongoose = require('mongoose');
-const User = require('./models/User');
-var cors = require('cors');
-const CookieParser = require('cookie-parser');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 // Environment Variables
 const port = process.env.PORT
 
-//for encryption of password 
-const bcryptSalt = bcrypt.genSaltSync(10);
+const nlpApp = Express();
 
-const jwtSecret = "jwindjncidwncjenjin";
-
-nlpApp.use(Express.json());
-nlpApp.use(CookieParser());
-
-//nlpApp.use(cors());
-
-
-nlpApp.use(cors({
-    credentials: true,
-    origin: 'http://localhost:3001',
-  }));
-
-
-//connect to mongoose using url
-mongoose.connect(process.env.MONGO_URL);
-
-nlpApp.get('/profile', (req,res) => {
-    //verify token --> get user data 
-    const {token} = req.cookies;
-    if (token) {
-        jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-            if (err) throw err;
-            const {name, email,_id} = await User.findById(userData.id);
-            res.json({name,email,_id});
-        })
-    } else {
-        res.json(null);
-    }
-})
-
-//register user 
-nlpApp.post('/register', async (req, res) => {
-    const {name, email, password} = req.body;
-    try {
-        const user = await User.create({
-            name, email, 
-            password: bcrypt.hashSync(password, bcryptSalt)
-        })
-        res.json(user);
-    } catch (e) {
-        res.status(422).json(e);
-        console.log(e);
-    }
-})
-
-//user log in --> check 
-nlpApp.post('/login', async (req,res) => {
-    //get name and password form the user 
-    const {email, password} = req.body;
-    //find user using email
-    const user = await User.findOne({email});
-
-    console.log()
-
-    console.log(user);
-
-    //if userexists in database
-    if (user) {
-        //check if password is the same
-        const pass = bcrypt.compareSync(password, user.password);
-        if (pass) {
-            //create cookie 
-            jwt.sign({
-                email:user.email, 
-                id:user._id}
-                , jwtSecret, {}, (err, token) => {
-                if (err) throw err;
-                res.cookie('token', token, {httpOnly: true, sameSite: 'None', secure: true}).json(user);
-            });
-        }
-        else {
-            res.status(422).json('pass not ok');
-        }
-    } else {
-        res.json('not found');
-    }
-})
-
-nlpApp.post('/logout', (req,res) => {
-    res.cookie('token', "").json(true);
-})
+var cors = require('cors')
+nlpApp.use(cors());
 
 // Multer Middleware to handle File Uploads
 // Set up multer instance
@@ -134,27 +46,27 @@ nlpApp.post('/upload-article', upload.single('pdfFile'), (request, response) => 
     }
 
     // Call the Summary Function in Python
-    const pythonProcess = spawn('python3.9', [summarisePyFile, uploadedArticle.path]);
+    // const pythonProcess = spawn('python3.9', [summarisePyFile, uploadedArticle.path]);
 
-    pythonProcess.stdout.on('data', (data) => {
-        // Do something with the data returned from python script
-        console.log(data);
-        pythonProcess.stdout.removeListener('data', onData);
-    });
+    // pythonProcess.stdout.on('data', (data) => {
+    //     // Do something with the data returned from python script
+    //     console.log(data);
+    //     pythonProcess.stdout.removeListener('data', onData);
+    // });
 
-    function onData(data) {
-        // Do something with the data returned from python script
-        console.log(data);
-    }
+    // function onData(data) {
+    //     // Do something with the data returned from python script
+    //     console.log(data);
+    // }
 
-    // var options = {
-    //     mode: 'text',
-    //     pythonPath: "/opt/homebrew/bin/python3.9",
-    //     pythonOptions: ['-u'],
-    //     // make sure you use an absolute path for scriptPath
-    //     scriptPath: "/Users/ngxua/WebDevelopmentProjects/iNTUitive-9.0/server/nlp-gensim",
-    //     args: [uploadedArticle.filename]
-    // };
+    var options = {
+        mode: 'text',
+        pythonPath: "/opt/homebrew/bin/python3.9",
+        pythonOptions: ['-u'],
+        // make sure you use an absolute path for scriptPath
+        scriptPath: "/Users/ngxua/WebDevelopmentProjects/iNTUitive-9.0/server/nlp-gensim",
+        args: [uploadedArticle.filename]
+    };
     
     // console.log("Before Python");
     // // create a new PythonShell instance
@@ -182,29 +94,29 @@ nlpApp.post('/upload-article', upload.single('pdfFile'), (request, response) => 
     // // End the PythonShell instance explicitly
     // pythonShell.end();
 
-    // pythonShell.run(summarisePyFile, options, (err, result) => {
-    //     console.log("Python was here");
-    //     if (err) {
-    //         console.log(err);
-    //         response.status(500).send("Error 500: Server Error");
-    //     }
-    //     else {
-    //         console.log(result);
-    //         const summary = result.join(' '); // join the list of sentences into a single string
-    //         response.status(200).json({
-    //             uploadSuccess: true,
-    //             title: uploadedArticle.filename,
-    //             summary: summary // send the summary as a property in the response JSON object
-    //         });
-    //     }
+    PythonShell.run("", options, (err, result) => {
+        console.log("Python was here");
+        if (err) {
+            console.log(err);
+            response.status(500).send("Error 500: Server Error");
+        }
+        else {
+            console.log(result);
+            const summary = result.join(' '); // join the list of sentences into a single string
+            response.status(200).json({
+                uploadSuccess: true,
+                title: uploadedArticle.filename,
+                summary: summary // send the summary as a property in the response JSON object
+            });
+        }
 
-    //     // end the PythonShell instance explicitly
-    //     // pythonShell.end(function (err) {
-    //     //     if (err) {
-    //     //         console.log(err);
-    //     //     }
-    //     // });
-    // })
+        // end the PythonShell instance explicitly
+        // pythonShell.end(function (err) {
+        //     if (err) {
+        //         console.log(err);
+        //     }
+        // });
+    })
 
     
     
