8705162eb80c7f7301babd05c4fcf475c04dfdec
JingSiong <yujingsiong@gmail.com>
2023-02-25 16:13:23 +0800

    added register

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Layout from './Layout';
 //import Navbar from './components/Navbar';
 import HomePage from "./Pages/HomePage"
 import LoginPage from "./Pages/LoginPage"
+import RegisterPage from './Pages/RegisterPage';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Layout/>}>
           <Route index element={<HomePage/>} />
           <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/register" element={<RegisterPage/>}/>
         </Route>
     </Routes>
   ) 
